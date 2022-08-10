c24f46b18c1bf72b0bf28c78334c4711f1f361df
zkazharan <azka.azharan@gmail.com>
2022-08-11 00:22:51 +0700

    message-view-rev4

--- View/message-script.js
+++ View/message-script.js
@@ -5,11 +5,15 @@ let igImg = document.getElementById("ig-img");
 
 let body = document.getElementById("body");
 let message = document.getElementById("message");
+let msgWarnEle = document.getElementById("msg-warn");
 let igPrev = document.getElementById("ig-preview");
 let postButton = document.getElementById("post-button");
 
 let twtBtnIsClick = false;
 let igBtnIsClick = false;
+
+let msgValid = true;
+let msgWarn;
 
 twtBtn.addEventListener('mouseenter', twtMouseEnter);
 twtBtn.addEventListener('mouseleave', twtMouseLeave);
@@ -18,8 +22,8 @@ igBtn.addEventListener('mouseenter', igMouseEnter);
 igBtn.addEventListener('mouseleave', igMouseLeave);
 igBtn.addEventListener('click', igClick);
 
-function twtMouseEnter(){
-    if(twtBtnIsClick === false){
+function twtMouseEnter() {
+    if (twtBtnIsClick === false) {
         twtBtn.style.cursor = 'pointer';
         twtBtn.style.backgroundColor = '#3FA3FF';
         twtBtn.style.borderColor = '#FFFFFF';
@@ -30,13 +34,13 @@ function twtMouseEnter(){
         twtImg.style.filter = 'brightness(0) invert(1) drop-shadow(3px 3px 2px rgba(0, 0, 0, .2))';
         twtImg.style.animation = 'bounce 1.25s ease-out';
     }
-    else{
+    else {
         twtImg.style.animation = 'none';
     }
 }
 
-function twtMouseLeave(){
-    if(twtBtnIsClick === false){
+function twtMouseLeave() {
+    if (twtBtnIsClick === false) {
         twtBtn.style.cursor = 'default';
         twtBtn.style.backgroundColor = '#FFFFFF';
         twtBtn.style.borderColor = '#3FA3FF';
@@ -50,45 +54,44 @@ function twtMouseLeave(){
     }
 }
 
-function twtClick(){
-    if(igBtnIsClick){
-        igClick();
-        igBtnIsClick = false;
-    }
-
-    if(twtBtnIsClick === false){
+function twtClick() {
+    if (twtBtnIsClick === false) {
         twtBtn.style.backgroundColor = '#3FA3FF';
         twtBtn.style.borderColor = '#FFFFFF';
         twtBtn.style.boxShadow = 'var(--twtBS_b)';
         twtBtn.style.transform = 'scale(1.1)'
         twtBtn.style.marginTop = '0';
-        twtBtn.style.animation = 'transform 100ms linear'
+        twtBtn.style.animation = 'transform 100ms linear';
         twtImg.style.filter = 'brightness(0) invert(1) drop-shadow(3px 3px 2px rgba(0, 0, 0, .2))';
 
-        postButton.style.display = 'block';
-        message.style.height = 'fit-content';
-        body.style.height = 'var(--twtH)';
+        // postButton.style.display = 'block';
+        // message.style.height = 'fit-content';
+        // body.style.height = 'var(--twtH)';
 
         twtBtnIsClick = true;
+
+        showWarn();
     }
     else {
         twtBtn.style.backgroundColor = '#FFFFFF';
         twtBtn.style.borderColor = '#3FA3FF';
         twtBtn.style.boxShadow = 'var(--twtBS_b)';
         twtBtn.style.transform = 'scale(1)';
-        twtBtn.style.animation = 'transform 100ms linear'
+        twtBtn.style.animation = 'transform 100ms linear';
         twtImg.style.filter = 'none';
 
-        postButton.style.display = 'none';
-        message.style.height = 'var(--msheight)';
-        body.style.height = 'auto';
+        // postButton.style.display = 'none';
+        // message.style.height = 'var(--msheight)';
+        // body.style.height = 'auto';
 
         twtBtnIsClick = false;
+
+        showWarn();
     }
 }
 
-function igMouseEnter(){
-    if(igBtnIsClick === false){
+function igMouseEnter() {
+    if (igBtnIsClick === false) {
         igBtn.style.cursor = 'pointer';
         igBtn.style.backgroundColor = '#E25B5B';
         igBtn.style.borderColor = '#FFFFFF';
@@ -99,13 +102,13 @@ function igMouseEnter(){
         igImg.style.filter = 'brightness(0) invert(1) drop-shadow(3px 3px 2px rgba(0, 0, 0, .2))';
         igImg.style.animation = 'bounce 1.25s ease-out';
     }
-    else{
+    else {
         igImg.style.animation = 'none';
     }
 }
 
-function igMouseLeave(){
-    if(igBtnIsClick === false){
+function igMouseLeave() {
+    if (igBtnIsClick === false) {
         igBtn.style.cursor = 'default';
         igBtn.style.backgroundColor = '#FFFFFF';
         igBtn.style.borderColor = '#E25B5B';
@@ -119,30 +122,163 @@ function igMouseLeave(){
     }
 }
 
-function igClick(){
-    if(twtBtnIsClick){
-        twtClick();
-        twtBtnIsClick = false;
-    }
-
-    if(igBtnIsClick === false){
+function igClick() {
+    if (igBtnIsClick === false) {
         igBtn.style.backgroundColor = '#E25B5B';
         igBtn.style.borderColor = '#FFFFFF';
         igBtn.style.boxShadow = 'var(--igBS_b)';
         igBtn.style.transform = 'scale(1.1)'
         igBtn.style.marginTop = '0';
-        igBtn.style.animation = 'transform 100ms linear'
+        igBtn.style.animation = 'transform 100ms linear';
         igImg.style.filter = 'brightness(0) invert(1) drop-shadow(3px 3px 2px rgba(0, 0, 0, .2))';
 
-        igPrev.style.display = 'block'
-        postButton.style.display = 'block'
-        message.style.height = 'fit-content';
-        body.style.height = 'var(--igH)';
+        // igPrev.style.display = 'block';
+        // postButton.style.display = 'block';
+        // body.style.height = 'var(--igH)';
 
         igBtnIsClick = true;
 
-        let content_message = document.querySelector(".msg-input").value;
+        showWarn();
+    }
+    else {
+        igBtn.style.borderColor = '#E25B5B';
+        igBtn.style.backgroundColor = '#FFFFFF';
+        igBtn.style.boxShadow = 'var(--igBS_b)';
+        igBtn.style.transform = 'scale(1)';
+        igBtn.style.animation = 'transform 100ms linear';
+        igImg.style.filter = 'none';
+
+        // igPrev.style.display = 'none'
+        // postButton.style.display = 'none'
+        // body.style.height = 'auto';
+
+        igBtnIsClick = false;
+
+        showWarn();
+    }
+}
+
+function showWarn() {
+
+    if (twtBtnIsClick === true || igBtnIsClick === true) {
+
+        checkMsg();
+
+        if(!msgValid){
+
+            msgWarnEle.textContent = msgWarn;
+            msgWarnEle.style.display = 'block';
+            message.style.height = 'fit-content';
+
+        } else {
+
+            msgWarnEle.style.display = 'none';
+            message.style.height = 'var(--msheight)';
+
+        }
+
+    } else if (twtBtnIsClick === false && igBtnIsClick === false) {
+
+        msgWarnEle.style.display = 'none';
+        message.style.height = 'var(--msheight)';
+
+    }
+
+}
+
+function checkMsg() {
+    
+    let contentMessage = document.querySelector(".msg-input").value;
+    let accCharacters = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890 `~!@#$%^&*()-_=+[]{}\\|;:\'",<.>/?';
+    let accCharArr = accCharacters.split('');
+    let words = contentMessage.split(' ');
+    let chars = contentMessage.split('');
+    let wordLenValid = true;
+    let charValid = true;
+
+    words.forEach((val) => {
+
+        if (val.length > 25) {
+
+            wordLenValid = false;
+
+            return;
+        }
+
+    });
+
+    chars.forEach((val) => {
+
+        let charFound = false;
+
+        accCharArr.forEach((val2) => {
+
+            if (val === val2) {
+
+                charFound = true;
+
+                return;
+            }
+
+        });
+
+        if (!charFound) {
+
+            charValid = false;
+
+            return;
+        }
+
+    });
+
+    if (contentMessage === '') {
+
+        msgWarn = 'Message can\'t be empty!'
+        msgValid = false;
+
+        return;
+    } else if (contentMessage.length < 6) {
+
+        msgWarn = 'Your message is too short!'
+        msgValid = false;
+
+        return;
+    } else if (contentMessage.length > 270) {
+
+        msgWarn = 'Your message is too long!'
+        msgValid = false;
+
+        return
+    } else if (!wordLenValid) {
+
+        msgWarn = 'Your message is invalid!'
+        msgValid = false;
+
+        return;
+    } else if (!charValid) {
+
+        msgWarn = 'Your message is invalid!'
+        msgValid = false;
+
+        return;
+    }
+
+    msgWarn = '';
+    msgValid = true;
+
+}
+
+const igContentPrev = () => {
+
+    let api = 'https://usayit-api.herokuapp.com/api/igPreview';
+    let msgPreview;
+
+    let syncronize = setInterval(async () => {
+
+        let contentMessage = document.querySelector(".msg-input").value;
         let prevImage = document.getElementById("preview-img");
+        
+        
 
         fetch('https://usayit-api.herokuapp.com/api/igPreview', {
             method: 'POST',
@@ -150,22 +286,8 @@ function igClick(){
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({text: content_message})
+            body: JSON.stringify({text: contentMessage})
         }).then(response => response.json()).then(response => {prevImage.src = response.image});
-    }
-    else {
-        igBtn.style.borderColor = '#E25B5B';
-        igBtn.style.backgroundColor = '#FFFFFF';
-        igBtn.style.boxShadow = 'var(--igBS_b)';
-        igBtn.style.transform = 'scale(1)';
-        igBtn.style.animation = 'transform 100ms linear'
-        igImg.style.filter = 'none';
 
-        igPrev.style.display = 'none'
-        postButton.style.display = 'none'
-        message.style.height = 'var(--msheight)';
-        body.style.height = 'auto';
-
-        igBtnIsClick = false;
-    }
+    }, 3000)
 }
