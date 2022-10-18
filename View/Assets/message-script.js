d6e90c15ec45cc26e55551852e5b1b6915e56048
Mikail Asada <62200797+Asadaaaaa@users.noreply.github.com>
2022-10-18 13:05:12 +0700

    Update message-script.js

--- View/Assets/message-script.js
+++ View/Assets/message-script.js
@@ -387,7 +387,7 @@ const igContentPrev = async () => {
             continue;
         }
 
-        await fetch('https://usayit-api.otech.id/api/igPreview', {
+        await fetch('http://usayit-api.otech.id/api/igPreview', {
 
             method: 'POST',
             headers: {
@@ -430,7 +430,7 @@ postButton.addEventListener('click', async () => {
 
     await fetch('https://api.ipify.org?format=json').then(res => res.json()).then(res => userIp = res.ip);
 
-    await fetch('https://usayit-api.otech.id/api/sendMessage', {
+    await fetch('http://usayit-api.otech.id/api/sendMessage', {
 
         method: 'POST',
         headers: {
